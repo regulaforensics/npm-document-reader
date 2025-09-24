4733b31e07c59f0394c780c8bedaafd415b3e027
Jenkins <jenkins@regula.by>
2025-09-24 17:24:37 +0300

    8.3.310-beta

--- examples/react_native/metro.config.js
+++ examples/react_native/metro.config.js
@@ -1,7 +1,5 @@
 const config = require('expo/metro-config').getDefaultConfig(__dirname)
 config.watchFolders = [
-    __dirname + '/../core', // locally connected core module
-    __dirname + '/../btdevice', // locally connected btdevice module
     __dirname + '/node_modules'
 ]
 config.resolver.nodeModulesPaths = config.watchFolders
