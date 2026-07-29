9f20f7f2e0acfeca5c3d5c867883d671e0e7d1e2
Jenkins <jenkins@regula.by>
2026-07-29 13:46:21 +0300

    9.6.959-rc

--- examples/react_native/scripts/setup.sh
+++ examples/react_native/scripts/setup.sh
@@ -2,9 +2,18 @@
 set -e
 
 npm install
-expo prebuild
+
+if expo prebuild; then
+    rc=0
+else
+    rc=$?
+fi
+
+printf 'expo prebuild exit code: %d\n' "$rc"
+
+if (( rc != 0 )); then
+    exit "$rc"
+fi
 
 mkdir -p android/app/src/main/assets/Regula
 mv android/app/src/main/res/raw/db.dat android/app/src/main/assets/Regula/
-
-exit
