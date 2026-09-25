83d2f4e3e71f2fdec84645773e57551620462cf8
Jenkins <jenkins@regula.by>
2026-09-25 14:17:05 +0300

    9.9.1166-beta

--- examples/react_native/scripts/setup.sh
+++ examples/react_native/scripts/setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# Kill Metro instance at app's port.
+lsof -tiTCP:${npm_package_config_metroPort} -sTCP:LISTEN | xargs kill 2>/dev/null || true
+
 npm install
 rm -rf node_modules/expo/node_modules/expo-modules-core/android/.cxx # Temp fix of the bug in expo@58.0.0-preview.3
 # Run CocoaPods separately because Expo prebuild can return success even when pod installation fails.
--- react-native-document-reader.podspec
+++ react-native-document-reader.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = 'react-native-document-reader'
-  s.version      = '9.9.1165-beta'
+  s.version      = '9.9.1166-beta'
   s.summary      = 'Regula React Native plugin.'
   s.license      = 'commercial'
   s.authors      = { 'RegulaForensics' => 'support@regulaforensics.com' }
